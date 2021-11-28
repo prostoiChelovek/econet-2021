78751ce5df62728a2157eb813b73acbd64d6264a
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-11-28 15:43:21 +0500

    unwrap peripherals instead of using if-let

--- software/peripheral-controller/src/main.rs
+++ software/peripheral-controller/src/main.rs
@@ -28,31 +28,28 @@ fn main() -> ! {
         };
     set_print_channel(channels.up.0);
 
-    if let (Some(dp), Some(cp)) = (
-        pac::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the LED. On the Mini-F4 it's connected to pin PC13.
-        let gpioa = dp.GPIOA.split();
-        let mut led = gpioa.pa5.into_push_pull_output();
+    let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
 
-        // Set up the system clock. We want to run at 48MHz for this one.
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+    // Set up the LED. On the Mini-F4 it's connected to pin PC13.
+    let gpioa = dp.GPIOA.split();
+    let mut led = gpioa.pa5.into_push_pull_output();
 
-        // Create a delay abstraction based on general-pupose 32-bit timer TIM5
-        let mut delay = delay::Delay::new(cp.SYST, clocks);
+    // Set up the system clock. We want to run at 48MHz for this one.
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
 
-        loop {
-            // On for 1s, off for 1s.
-            led.set_high();
-            rprintln!("ON");
-            delay.delay_ms(1_000_u32);
-            led.set_low();
-            rprintln!("OFF");
-            delay.delay_us(1_000_000_u32);
-        }
+    // Create a delay abstraction based on general-pupose 32-bit timer TIM5
+    let mut delay = delay::Delay::new(cp.SYST, clocks);
+
+    loop {
+        // On for 1s, off for 1s.
+        led.set_high();
+        rprintln!("ON");
+        delay.delay_ms(1_000_u32);
+        led.set_low();
+        rprintln!("OFF");
+        delay.delay_us(1_000_000_u32);
     }
-
-    loop {}
 }
+
