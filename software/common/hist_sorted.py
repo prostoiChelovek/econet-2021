8f395e25bd4f1c5f87b29ad7d5d61e92491def30
TimofeiMenshikov <tim777men@gmail.com>
2021-12-26 16:35:46 +0500

    убрать лишнюю функцию typing

--- software/common/hist_sorted.py
+++ software/common/hist_sorted.py
@@ -1,50 +1,33 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-#import seaborn as sns
 
 
-def typing (name):
-    type = ''
-    i = 0
-    j = -1
-    for i in range (len(name)):
-        try:
-            j = int(name[i])
-
-        except :
-            j = -1
-            type += name[i]
-            i += 1
-            
-        if j > -1:
-            return type
-
-
-def image (name):
-    return cv.imread('/home/tima/rubbish/{}/{}.jpg'.format(typing(name),name))
+def load_image (name, num=0): 
+    return cv.imread(f'/home/tima/rubbish/{name}/{name}{num}.jpg')
 
 
 def subplot (j, k, col, histx, histy, name, r):
     ax[j][k].plot(histx,histy,color = col)
-    #ax[j][k] = sns.lineplot(data=histy,color = col)
-    #ax[j, k].set_title('{} {}'.format(name, col))
+    if j == 0:
+        ax[j, k].set_title(name)
+    
     ax[j][k].set_xticklabels(histx)
     plt.setp(ax[j][k].get_xticklabels(), rotation = r, horizontalalignment='right', fontsize='x-small')
 
 
 mask = None
 
-name0 = 'paper0'
-name1 = 'tin0'
-name2 = 'bottle0'
+name0 = 'paper'
+name1 = 'tin'
+name2 = 'bottle'
 
 fig , ax = plt.subplots(nrows = 3, ncols = 3, figsize = (25,25))
 fig.suptitle('rubbish')
 
 bgr = ('b', 'g', 'r')
 names = (name0,name1,name2)
-images = list(map(image,names))
+images = list(map(load_image,names))
 
 for k, img in enumerate(images):
     for j, col in enumerate (bgr):
