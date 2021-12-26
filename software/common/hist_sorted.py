1c79cca71e406e2b534d9fbec689c5d7b9f08720
TimofeiMenshikov <tim777men@gmail.com>
2021-12-26 18:18:21 +0500

    переделать отображение гистограммы

--- software/common/hist_sorted.py
+++ software/common/hist_sorted.py
@@ -23,25 +23,55 @@ def create_hist(img, channel):
                        histSize=[256],
                        ranges=[0, 256])
                            
-    return sort_hist(hist[:NUM_SAMPLES])
+    return sort_hist(hist)[:NUM_SAMPLES]
 
+
+def create_hists(images):
+    return  [ 
+        [
+            create_hist(img, channel) 
+            for channel in range(3)
+        ] for img in images
+    ]
     
-    ax[j][k].set_xticklabels(histx)
-    plt.setp(ax[j][k].get_xticklabels(), rotation = r, horizontalalignment='right', fontsize='x-small')
+
+def create_plot_data(hists):
+    res = []
+    for image_hists in hists:
+        image_data = []
+        for hist in image_hists:
+            histx = [str(x[1]) for x in hist]
+            histy = [x[0] for x in hist]
+
+            image_data.append((histx, histy))
+        res.append(image_data)
+
+    return res
+
+
+def plot_hist(axis, histx, histy, color):
+    axis.plot(histx, histy, color)
+    axis.set_xticklabels(histx)
+    plt.setp(axis.get_xticklabels(),
+             rotation=LABLE_ROTATION,
+             horizontalalignment='right',
+             fontsize='x-small')
+
+
+def plot(axes, plot_data):
+    for col, image_data in enumerate(plot_data):
+        for row, (x, y) in enumerate(image_data):
+            axis = axes[col][row]
+            color = ('b', 'g', 'r')[col]  
+            plot_hist(axis, x, y, color)
 
 
 fig , ax = plt.subplots(nrows = 3, ncols = 3, figsize = (25,25))
 fig.suptitle('rubbish')
 
 images = list(map(load_image, NAMES))
+hists = create_hists(images)
+plot_data = create_plot_data(hists)
+plot(ax, plot_data)
 
-for k, img in enumerate(images):
-    for j, col in enumerate(('b', 'g', 'r')):
-        hist = create_hist(img, j)
-
-        histx = [str(x[1]) for x in hist]
-        histy = [x[0] for x in hist]
-        
-        subplot(j, k, col, histx, histy, name=NAMES[k], r=50)
-    
 plt.show()
