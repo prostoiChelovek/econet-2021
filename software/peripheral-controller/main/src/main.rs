9207fe639066c4ea3f73712d219d9bf628a7cb8b
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-01-05 23:09:36 +0500

    start using RTIC

--- software/peripheral-controller/main/src/main.rs
+++ software/peripheral-controller/main/src/main.rs
@@ -1,96 +1,91 @@
 #![no_main]
 #![no_std]
 
-use core::cell::RefCell;
-use cortex_m::interrupt::Mutex;
-use cortex_m_rt::entry;
-
-use rtt_target::{rprintln, rtt_init, set_print_channel};
 use panic_probe as _;
 
-use fugit::TimerDurationU32;
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [USART1])]
+mod app {
+    use rtt_target::{rtt_init, set_print_channel};
 
-use stm32f4xx_hal::{
-    prelude::*, delay, timer::{CounterUs, Timer}, pac, pac::TIM2,
-    qei::Qei
-};
+    use stm32f4xx_hal::{
+        prelude::*,
+        pac, pac::TIM1,
+        gpio::{Output, Pin, PushPull},
+        timer::{monotonic::MonoTimer, Timer},
+        pwm::{PwmChannel, C1},
+        qei::Qei
+    };
 
-use motor::{Motor, SetSpeed, RotationDirection};
-use dc_motor::{TwoPinSetDirection, PwmSetSpeed};
-use rotary_encoder::RotaryEncoder;
+    use motor::{Motor, SetSpeed};
+    use dc_motor::{TwoPinSetDirection, PwmSetSpeed};
+    use rotary_encoder::RotaryEncoder;
 
-type MicrosTimer = CounterUs<TIM2>;
-static MICROS_TIMER: Mutex<RefCell<Option<MicrosTimer>>> = Mutex::new(RefCell::new(None));
+     #[monotonic(binds = TIM2, default = true)]
+    type MicrosecMono = MonoTimer<pac::TIM2, 1_000_000>;
 
-fn micros() -> u32 {
-    static mut LOCAL_MICROS_TIMER: Option<MicrosTimer> = None;
+    #[shared]
+    struct Shared { }
 
-    unsafe {
-        let tim = LOCAL_MICROS_TIMER.get_or_insert_with(|| {
-            cortex_m::interrupt::free(|cs| { 
-                MICROS_TIMER.borrow(cs).replace(None).unwrap()
-            })
-        });
-        
-        tim.now().ticks()
+    // this should be illegal
+    type MyMotor = Motor<TwoPinSetDirection<Pin<Output<PushPull>, 'B', 10_u8>, Pin<Output<PushPull>, 'B', 4_u8>>, PwmSetSpeed<PwmChannel<TIM1, C1>>>;
+    #[local]
+    struct Local {
+        motor: MyMotor
     }
-}
 
-#[entry]
-fn main() -> ! {
-    let channels = 
-        rtt_init! {
-            up: {
-                0: {
-                    size: 1024
-                    name: "Terminal"
+     #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let channels = 
+            rtt_init! {
+                up: {
+                    0: {
+                        size: 1024
+                            name: "Terminal"
+                    }
                 }
-            }
-            down: {
-                0: {
-                    size: 16
-                    name: "Terminal"
-                }
-            }
-        };
-    set_print_channel(channels.up.0);
+                    down: {
+                        0: {
+                            size: 16
+                                name: "Terminal"
+                        }
+                    }
+            };
+        set_print_channel(channels.up.0);
 
-    let dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
 
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(84.mhz()).freeze();
+        let gpioa = ctx.device.GPIOA.split();
+        let gpiob = ctx.device.GPIOB.split();
 
-    let mut micros_timer = Timer::new(dp.TIM2, &clocks).counter_us();
-    micros_timer.start(TimerDurationU32::from_ticks(u32::max_value())).unwrap();
-    cortex_m::interrupt::free(|cs| {
-        *MICROS_TIMER.borrow(cs).borrow_mut() = Some(micros_timer);
-    });
+        let (in_1, in_2) = (gpiob.pb10.into_push_pull_output(), gpiob.pb4.into_push_pull_output());
+        let en_pin = gpioa.pa8.into_alternate();
+        let en_pwm = Timer::new(ctx.device.TIM1, &clocks).pwm(en_pin, 2.khz());
 
-    let gpioa = dp.GPIOA.split();
-    let gpiob = dp.GPIOB.split();
+        let directin = TwoPinSetDirection::new(in_1, in_2);
+        let speed = PwmSetSpeed::new(en_pwm, 25);
+        let motor = Motor::new(directin, speed);
 
-    let (in_1, in_2) = (gpiob.pb10.into_push_pull_output(), gpiob.pb4.into_push_pull_output());
-    let en_pin = gpioa.pa8.into_alternate();
-    let en_pwm = Timer::new(dp.TIM1, &clocks).pwm(en_pin, 2.khz());
+        let encoder_pins = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate());
+        let encoder_timer = ctx.device.TIM5;
+        let qei = Qei::new(encoder_timer, encoder_pins);
+        let mut _encoder = RotaryEncoder::new(qei, 1440_f32);
 
-    let directin = TwoPinSetDirection::new(in_1, in_2);
-    let speed = PwmSetSpeed::new(en_pwm, 25);
-    let mut motor = Motor::new(directin, speed);
+        let mono = Timer::new(ctx.device.TIM2, &clocks).monotonic();
 
-    let encoder_pins = (gpioa.pa0.into_alternate(), gpioa.pa1.into_alternate());
-    let encoder_timer = dp.TIM5;
-    let qei = Qei::new(encoder_timer, encoder_pins);
-    let mut encoder = RotaryEncoder::new(qei, 1440_f32);
+        task1::spawn().ok();
 
-    let mut delay = delay::Delay::new(cp.SYST, &clocks);
+        (
+            Shared { },
+            Local {
+                motor
+            },
+            init::Monotonics(mono),
+        )
+    }
 
-    loop {
-        encoder.update(0.1);
-        let pos = encoder.get_position();
-        let vel = encoder.get_velocity();
-        rprintln!("{} {}", pos, vel);
-
-        delay.delay_ms(100_u32);
+    #[task(local = [motor])]
+    fn task1(cx: task1::Context) {
+        cx.local.motor.set_speed(100);
     }
 }
